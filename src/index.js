369210a5609c8d543ed4eb55fbda2f7b6cbb6afb
A7MED <a7medabdulshafi@gmail.com>
2023-04-19 01:38:12 +0300

    ffffffffff

--- src/index.js
+++ src/index.js
@@ -6,8 +6,8 @@ import { DarkModeContextProvider } from "./context/darkModeContext";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Provider } from 'react-redux';
 import { store } from './app/store';
-import { disableReactDevTools } from '@fvilers/disable-react-devtools';
-if (process.env.NODE_ENV === 'production') disableReactDevTools()
+// import { disableReactDevTools } from '@fvilers/disable-react-devtools';
+// if (process.env.NODE_ENV === 'production') disableReactDevTools()
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
